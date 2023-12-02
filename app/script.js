9b7aaf415895a2df4491b6af0fca7be6752bd370
Gulab Ahmad <125208326+GulabAhmad@users.noreply.github.com>
2023-12-02 00:50:38 -0800

    Add files via upload

---
+++ app/script.js
@@ -0,0 +1,12 @@
+// navbar togglemenu start
+import navLinks from "react";
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.right = "0";
+}
+
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
+// navbar togglemenu end
